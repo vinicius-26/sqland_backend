a3d3387741ec104b3ac598f0b03fe503c431f8cd
vinicius-26 <vistephano123@gmail.com>
2024-10-20 00:55:36 -0300

    Ajustando o CORS da aplicação

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,18 @@ import cors from 'cors';
 
 const app = express();
 
-// Configura o middleware CORS
+// Lista de domínios permitidos
+const allowedOrigins = ['http://localhost:3000', 'https://sqland.vercel.app'];
+
 app.use(cors({
-  origin: 'http://localhost:3000', // Permite apenas solicitações do frontend
+  origin: function (origin, callback) {
+    // Permite a requisição se o domínio estiver na lista permitida ou se não houver origem (caso de certas requisições internas)
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST'], // Métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
 }));
@@ -14,5 +23,7 @@ app.use(cors({
 app.use(express.json());
 app.use(routes);
 
-// localhost:3080
-app.listen(3080)
+// Porta onde o servidor escutará
+app.listen(3080, () => {
+  console.log('Server running on port 3080');
+});
